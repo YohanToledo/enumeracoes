8c2f9948e8920bd99b0f593d9a2364feb8e5aac7
Yohan Toledo <yohan.gabriel.bernart@gmail.com>
2021-03-23 18:10:48 -0300

    enum-v1.0

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Date; 
+
+import entities.HourContract;
+import entities.enums.WorkerLevel;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+
+	}
+
+}
